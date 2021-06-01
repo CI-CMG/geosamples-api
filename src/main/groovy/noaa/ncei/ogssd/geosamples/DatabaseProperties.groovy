d36c171387d70cfc584d64dc7013b0e6ea27d204
jcc <john.c.cartwright@comcast.net>
2021-06-01 13:02:21 -0600

    remove conditional database properties beans in favor of using properties w/ constructor injection

--- src/main/groovy/noaa/ncei/ogssd/geosamples/DatabaseProperties.groovy
+++
@@ -1,8 +0,0 @@
-package noaa.ncei.ogssd.geosamples
-
-interface DatabaseProperties {
-    static final String SCHEMA
-    static final String DIALECT
-    static final String SAMPLE_TABLE = 'curators_sample_tsqp'
-    static final String INTERVAL_TABLE = 'curators_interval'
-}
