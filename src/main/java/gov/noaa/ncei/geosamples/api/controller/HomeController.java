3168a74033726b709ddcdde7ae4f9ef64a3fc53b
Chris Slater <6004777+electricsam@users.noreply.github.com>
2022-11-17 08:44:07 -0700

    attempt to set cors on non slash root

--- src/main/java/gov/noaa/ncei/geosamples/api/controller/HomeController.java
+++ src/main/java/gov/noaa/ncei/geosamples/api/controller/HomeController.java
@@ -1,11 +1,13 @@
 package gov.noaa.ncei.geosamples.api.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 //@RequestMapping("/geosamples-api")
 class HomeController {
+    @CrossOrigin
     @GetMapping("/")
     public String home() {
         return "index";
