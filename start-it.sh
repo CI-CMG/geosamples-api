31c99e9547b2d2b178fbe5d748f2d386333c6e7b
Chris Slater <6004777+electricsam@users.noreply.github.com>
2025-04-15 11:48:33 -0600

    prepare for open source

--- start-it.sh
+++ start-it.sh
@@ -6,5 +6,4 @@ if [[ -z "$MB_WORK_DIR" ]]; then
    MB_WORK_DIR="$(pwd)/test-dir/work"
 fi
 
-
-docker run -d --name geosamples-api-test-1 -p 15215:1521 cirescmg/crowbar-db:20210416
+docker run --name geosamples-api-test-1 -p 15215:1521 -v $(pwd)/src/test/resources/db-startup:/opt/oracle/scripts/startup -d container-registry.oracle.com/database/free:23.6.0.0-lite
